65ff9e654b1c3e73ad72a2461d59e26970dc0060
KattyOsorio <167355129+KattyOsorio@users.noreply.github.com>
2024-10-18 13:31:36 -0500

    Create ciclos2.cs

---
+++ C#/ciclos/ciclos2.cs
@@ -0,0 +1,8 @@
+ int suma = 0;
+
+        for (int s = 1; s <= 20; s++)
+        {
+            suma += s;
+        }
+
+        Console.WriteLine($"La suma de los primeros 20 números naturales es: {suma}");
