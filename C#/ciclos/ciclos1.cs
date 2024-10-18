7de45cdf7e0ccea8c1a01bfd7a0925045071e991
KattyOsorio <167355129+KattyOsorio@users.noreply.github.com>
2024-10-18 13:29:17 -0500

    Create ciclos1.cs

---
+++ C#/ciclos/ciclos1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Escriba un programa para mostrar los primeros 10 números naturales.
+
+        for (int s = 1; s <= 10; s++)
+        {
+            Console.WriteLine(s); 
+        }
+    }
+}
